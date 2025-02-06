ed5f36edb17d63aaa3f4b43e61a63a534537846e
nazimudheen-lifisin <fe02@lifisin.com>
2025-02-06 15:17:34 +0530

    code updated

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -359,9 +359,55 @@ export default function Home() {
                 </div>
                 <h2 className="text-center mb-5">Our Students Say!</h2>
 
-                <div className="flex gap-3">
+                <div className="relative w-11/12 mx-auto">
+                    <Carousel
+                        showThumbs={false}
+                        showStatus={false}
+                        showIndicators={false}
+                        infiniteLoop={true}
+                        centerMode={true}
+                        interval={2000}
+                        autoPlay
+                        showArrows={false}
+                        centerSlidePercentage={33.3}
+                        dynamicHeight={false}
+                        emulateTouch={true}
+                    >
+                        <div className="relative bg-white rounded-lg shadow-lg w-full mx-4">  {/* Added mx-4 here */}
+                            <div className="flex flex-col justify-center items-center overflow-hidden">
+                                <img src={CarouselOne} alt="image.png" className="h-16 max-w-16 object-cover rounded-full" />
+                                <h1>Joseph</h1>
+                                <p>Profession</p>
+                                <div className="bg-[#32a8a2] py-2 px-2">
+                                    <p className="text-[#fff]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ab facilis eos! Soluta corrupti inventore quos dolorum quam nemo praesentium voluptas aliquam, fugiat,</p>
+                                </div>
+                            </div>
+                        </div>
 
+                        <div className="relative w-full bg-white rounded-lg shadow-lg mx-4">  {/* Added mx-4 here */}
+                            <div className="flex flex-col justify-center items-center overflow-hidden">
+                                <img src={CarouselOne} alt="image.png" className="h-16 max-w-16 object-cover rounded-full" />
+                                <h1>King Lee</h1>
+                                <p>Profession</p>
+                                <div className="bg-[#32a8a2] py-2 px-2">
+                                    <p className="text-[#fff]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ab facilis eos! Soluta corrupti inventore quos dolorum quam nemo praesentium voluptas aliquam, fugiat,</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="relative w-full bg-white rounded-lg shadow-lg mx-4">  {/* Added mx-4 here */}
+                            <div className="flex flex-col justify-center items-center overflow-hidden">
+                                <img src={CarouselOne} alt="image.png" className="h-16 max-w-16 object-cover rounded-full" />
+                                <h1>Nazim</h1>
+                                <p>Profession</p>
+                                <div className="bg-[#32a8a2] py-2 px-2">
+                                    <p className="text-[#fff]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ab facilis eos! Soluta corrupti inventore quos dolorum quam nemo praesentium voluptas aliquam, fugiat,</p>
+                                </div>
+                            </div>
+                        </div>
+                    </Carousel>
                 </div>
+
 
             </div>
 
@@ -531,8 +577,10 @@ export default function Home() {
                 <span className="block text-center text-xs">&copy; Lifisin Technologies, All right reservered</span>
             </footer>
 
-            <div className="h-16 w-16 bg-amber-400 fixed bottom-8 right-8">
-                <svg fill="#000000" width="800px" height="800px" viewBox="0 0 24 24" id="up-arrow" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color"><path id="primary" d="M15.71,5.29l-3-3a1,1,0,0,0-1.42,0l-3,3A1,1,0,0,0,9.71,6.71L11,5.41V21a1,1,0,0,0,2,0V5.41l1.29,1.3a1,1,0,0,0,1.42,0A1,1,0,0,0,15.71,5.29Z" style="fill: rgb(0, 0, 0);"></path></svg>
+            <div className="cursor-pointer h-16 w-16 bg-[#32a8a6] justify-center flex items-center fixed bottom-8 right-8">
+                <svg width="38px" height="38px" viewBox="0 0 24 24" fill="#fff" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C12.2652 3 12.5196 3.10536 12.7071 3.29289L19.7071 10.2929C20.0976 10.6834 20.0976 11.3166 19.7071 11.7071C19.3166 12.0976 18.6834 12.0976 18.2929 11.7071L13 6.41421V20C13 20.5523 12.5523 21 12 21C11.4477 21 11 20.5523 11 20V6.41421L5.70711 11.7071C5.31658 12.0976 4.68342 12.0976 4.29289 11.7071C3.90237 11.3166 3.90237 10.6834 4.29289 10.2929L11.2929 3.29289C11.4804 3.10536 11.7348 3 12 3Z" fill="#fff" />
+                </svg>
             </div>
 
         </div>
